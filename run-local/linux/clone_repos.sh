1082efbdd5afdd7e419b88bfcd1b3b89928c1dc6
arj <arj@informatik.uni-kiel.de>
2016-07-05 12:30:15 +0200

    add clone repos

---
+++ run-local/linux/clone_repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../../..
+
+git clone https://github.com/a-johanson/oceantea-visualization-gateway.git
+git clone https://github.com/a-johanson/oceantea-auth.git
+git clone https://github.com/a-johanson/oceantea-vector-time-series.git
+git clone https://github.com/a-johanson/oceantea-scalar-time-series.git
+git clone https://github.com/a-johanson/oceantea-time-series-conversion.git
+
+cd oceantea-build/run-local/linux
