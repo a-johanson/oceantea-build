00b5395fb5c7ec052869c060c95e52823aebdd0d
arj <arj@informatik.uni-kiel.de>
2016-07-05 12:36:51 +0200

    add pull repos

---
+++ run-local/linux/pull_repos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../../../oceantea-visualization-gateway
+git pull
+cd ../oceantea-auth
+git pull
+cd ../oceantea-scalar-time-series
+git pull
+cd ../oceantea-time-series-conversion
+git pull
+cd ../oceantea-vector-time-series
+git pull
+cd ../oceantea-build/run-local/linux
