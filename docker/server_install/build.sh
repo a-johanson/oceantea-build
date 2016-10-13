15e1f17de3555a73646cdc9cbf89414ea04c19bd
arj <arj@informatik.uni-kiel.de>
2016-10-13 18:55:04 +0200

    update docker scripts

--- docker/server_install/build.sh
+++ docker/server_install/build.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-function pullImage {
-	docker pull ajohanson/$1
+echo "Cleaning up build files..."
+rm -rf ./build
+mkdir ./build
+cd ./build
+
+function rebuildImage {
+	echo "Removing old image $1..."
+	docker rm -f $1-inst
+	docker rmi $1
+	echo "Cloning repo $1..."
+	git clone https://github.com/a-johanson/$1.git
+	echo "Building image $1..."
+	docker build -t $1 ./$1/
 }
+# main
+rebuildImage oceantea-auth
+rebuildImage oceantea-time-series-conversion
+rebuildImage oceantea-spatial-analysis
+rebuildImage oceantea-vector-time-series
+rebuildImage oceantea-scalar-time-series
+rebuildImage oceantea-visualization-gateway
+# plugin
+rebuildImage oceantea-coral-analysis
 
-pullImage oceantea-auth
-pullImage oceantea-time-series-conversion
-pullImage oceantea-spatial-analysis
-pullImage oceantea-vector-time-series
-pullImage oceantea-scalar-time-series
-pullImage oceantea-visualization-gateway
-
-pullImage oceantea-coral-analysis
 
 
-
-function runContainer {
-	if [[ "$(docker ps -a | grep $1-inst)" == "" ]]; then
-		echo "Container $1-inst does not exist"
-		docker run -d --restart=always ${@:2} --name="$1-inst" -t ajohanson/$1
-	else 
-		echo "Container $1-inst already exists"
-		docker start $1-inst
-	fi
+function runContainer {\
+	echo "Starting container $1-inst..."
+	docker run -d --restart=always ${@:2} --name="$1-inst" -t $1
 }
 
 runContainer oceantea-coral-analysis -v /dockervol/oceantea/coral_analysis:/usr/src/app/data
